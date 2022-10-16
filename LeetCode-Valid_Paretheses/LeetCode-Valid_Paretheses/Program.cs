5a60e1a6d99f4adf2d6957f207ce3c95cb50a440
wonhyuk cho <78832258+zzangzwh@users.noreply.github.com>
2022-10-15 22:09:14 -0600

    Add files via upload

---
+++ LeetCode-Valid_Paretheses/LeetCode-Valid_Paretheses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Valid_Paretheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine( ValidOne("{}{)"));
+            Console.ReadKey();
+        }
+        private static bool ValidOne(string s)
+        {
+            int n = 0;
+            while(s.Length != n)
+            {
+                n = s.Length;
+                s = s.Replace("()", "");
+                s = s.Replace("{}", "");
+                s = s.Replace("[]", "");
+            }
+            if (s.Length == 0) return true;
+            else
+               return false;
+        }
+    }
+}
